7cb7492214b25e8fb55c776b7d0241e80e21d24e
firyuza <firyuza.badalova@gmail.com>
2023-11-14 12:56:39 -0500

    Created Task1_Solution.java

---
+++ src/week_01/Task1_Solution.java
@@ -0,0 +1,27 @@
+package week_01;
+
+public class Task1_Solution {
+    public static void main(String[] args) {
+
+        System.out.println(Finra(100));
+    }
+    public static String Finra(int num) {
+
+        String result = "";
+
+        for (int i = 1; i <= num; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                result = "FINRA";
+            } else if (i % 5 == 0) {
+                result = "RA";
+            } else if (i % 3 == 0) {
+                result = "FIN";
+            } else
+                result = i + "";
+            System.out.print(result + " ");
+        }
+        return result;
+
+    }
+}
