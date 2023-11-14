fe891d040bc1ee301ab868422aa3c96e692e2de5
srestr53 <sangil9091@gmail.com>
2023-11-14 09:57:51 -0800

    added solution for Even and Odd number

---
+++ src/week_01/santiagoSolutions/EvenAndOddNumber.java
@@ -0,0 +1,21 @@
+package week_01.santiagoSolutions;
+
+public class EvenAndOddNumber {
+
+    public static void main(String[] args) {
+
+        oddOrEven(5);
+
+    }
+
+    public static void oddOrEven(int num) {
+        if (num == 0) {
+            System.out.println("Even number");
+
+        } else {
+            System.out.println(" Odd number");
+        }
+    }
+}
+/* Write a method that can */
+
