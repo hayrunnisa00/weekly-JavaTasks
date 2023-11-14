3d1f007502dd7f492c02f73086f9f35cec247e06
hayrunnisa00 <apaydinhayrunnisa@gmail.com>
2023-11-14 14:22:33 -0500

    updated Finra.java

--- src/week_01/nisa_solution/Finra.java
+++ src/week_01/nisa_solution/Finra.java
@@ -1,4 +1,31 @@
 package week_01.nisa_solution;
 
 public class Finra {
+    public static void main(String[] args) {
+
+        finra();
+
+    }
+
+
+    public static void finra(){
+
+        for (int i = 1; i <= 30; i++) {
+
+            if (i % 5 == 0 && i % 3 == 0){
+                System.out.print("FINRA ");
+            }else if (i % 3 == 0){
+                System.out.print("FIN ");
+            } else if (i % 5 == 0) {
+                System.out.print("RA ");
+            }else {
+                System.out.print(i + " ");
+            }
+
+
+        }
+
+    }
+
+
 }
