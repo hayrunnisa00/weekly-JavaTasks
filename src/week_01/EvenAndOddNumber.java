831ca5bd178322efdb740268afef3474d522b05b
Lokmanbzd <l.bouzerde@gmail.com>
2023-11-14 12:15:14 -0600

    Updated task 3 solution

---
+++ src/week_01/EvenAndOddNumber.java
@@ -0,0 +1,22 @@
+package week_01;
+
+public class EvenAndOddNumber {
+
+    public static void main(String[] args) {
+
+        oddOrEven(5);
+
+    }
+
+    public static void oddOrEven(int num) {
+        if (num == 0) {
+            System.out.println("Even number");
+
+        } else {
+            System.out.println(" Odd number");
+        }
+
+
+    }
+
+}
