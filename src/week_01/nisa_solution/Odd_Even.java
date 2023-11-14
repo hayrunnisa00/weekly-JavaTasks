1ffd88c0856218abe6cb50a1ced3221561288313
hayrunnisa00 <apaydinhayrunnisa@gmail.com>
2023-11-14 14:28:06 -0500

    updated Odd_Even.java

--- src/week_01/nisa_solution/Odd_Even.java
+++ src/week_01/nisa_solution/Odd_Even.java
@@ -1,4 +1,21 @@
 package week_01.nisa_solution;
 
 public class Odd_Even {
+    public static void main(String[] args) {
+
+        identify(5);
+        identify(6);
+
+    }
+
+    public static void identify(int number){
+        if (number % 2 == 0){
+            System.out.println("Even");
+        }else {
+            System.out.println("Odd");
+        }
+    }
+
+
+
 }
