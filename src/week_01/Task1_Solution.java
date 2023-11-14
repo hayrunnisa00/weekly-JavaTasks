1a5112bc646108947f704a5f92098b604fd8754a
Jonny <jonny5891@yahoo.com>
2023-11-14 12:57:58 -0500

    created code for task 1

---
+++ src/week_01/Task1_Solution.java
@@ -0,0 +1,22 @@
+package week_01;
+
+public class Task1_Solution {
+    public static void oddOrEven(int input) {
+
+        if (input % 2 !=0) {
+            System.out.println("Odd");
+        }else{
+            System.out.println("Even");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        oddOrEven(3);
+
+
+
+    }
+
+}
+
