b6321c8e30797442d05426f83affe3ffd9d9bc88
Lokmanbzd <l.bouzerde@gmail.com>
2023-11-14 12:12:58 -0600

    Updated task 2 solution

--- src/week_01/DivideWithoutOperator.java
+++ src/week_01/DivideWithoutOperator.java
@@ -2,5 +2,19 @@ package week_01;
 
 public class DivideWithoutOperator {
 
+    public static void main(String[] args) {
 
+
+        int num1 = 50;
+        int num2 = 10;
+        int count = 0;
+
+        while (num1 >= num2) {
+            num1 -= num2;
+            count++;
+        }
+
+        System.out.println("count = " + count);
+
+    }
 }
