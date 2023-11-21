a1226b300b864a10fe2667dec7d6f12478556e1c
hayrunnisa00 <apaydinhayrunnisa@gmail.com>
2023-11-20 22:27:03 -0500

    created SwapNumber

---
+++ src/week_02/nisa_solutions/SwapNumber.java
@@ -0,0 +1,26 @@
+package week_02.nisa_solutions;
+
+public class SwapNumber {
+    public static void main(String[] args) {
+
+        int num1 = 4;
+        int num2 = 7;
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+
+
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+
+
+
+
+
+    }
+}
