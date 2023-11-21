a76fe2ef6f1bf33288c7c8825d216669a88ef60f
Lokmanbzd <l.bouzerde@gmail.com>
2023-11-21 11:52:19 -0600

    updated DivisibleBy3_5_15 class

--- src/week_02/DivisibleBy3_5_15.java
+++ src/week_02/DivisibleBy3_5_15.java
@@ -9,11 +9,11 @@ public class DivisibleBy3_5_15 {
         String  divisibleBy5 = "";
         for (int i = 1; i < 101; i++) {
 
-            if (i % 15 == 0 && i % 5 == 0 && i % 3 == 0){
+            if (i % 15 == 0){
                 divisibleBy15 += i+" ";
-            } else if (i % 3 == 0 && i % 15 != 0) {
+            } else if (i % 3 == 0) {
                 divisibleBy3 += i+" ";
-            } else if (i % 5 == 0 && i % 15 != 0) {
+            } else if (i % 5 == 0) {
                 divisibleBy5 += i+" ";
             }
 
