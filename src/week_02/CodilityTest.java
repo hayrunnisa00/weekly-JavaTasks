b819594ed29ece2994330591534ae7a96a5fe87f
Lokmanbzd <l.bouzerde@gmail.com>
2023-11-21 12:23:04 -0600

    updated CodilityTest class

--- src/week_02/CodilityTest.java
+++ src/week_02/CodilityTest.java
@@ -1,8 +1,36 @@
 package week_02;
 
+import java.util.Scanner;
+
 public class CodilityTest {
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an int number: ");
+        int n = scanner.nextInt();
+
+        String result ="";
+        for (int i = 1; i <= n; i++) {
+
+            if (i%2!=0 && i%3!=0 && i%5!=0){
+                result+=i;
+            }
+
+            if (i%2==0){
+                result+="Codility";
+            }
+            if (i%3==0 ) {
+                result+="Test";
+            }
+            if (i%5==0) {
+                result+="Coders";
+            }
+            System.out.println(result);
+            result="";
+        }
+
+
 
     }
 
