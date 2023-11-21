b384ebb1f7f5315ee33ea9d80829da8d5fc19bbf
hayrunnisa00 <apaydinhayrunnisa@gmail.com>
2023-11-21 12:56:04 -0500

    updated DivisibleBy_3_5_15.java

--- src/week_02/nisa_solutions/SwapNumber.java
+++ src/week_02/nisa_solutions/SwapNumber.java
@@ -3,15 +3,15 @@ package week_02.nisa_solutions;
 public class SwapNumber {
     public static void main(String[] args) {
 
-        int num1 = 4;
-        int num2 = 7;
+        int num1 = 4;  // a
+        int num2 = 7;  // b
 
         System.out.println("num1 = " + num1);
         System.out.println("num2 = " + num2);
 
-        num1 = num1 + num2;
-        num2 = num1 - num2;
-        num1 = num1 - num2;
+        num1 = num1 + num2;  // a = a + b
+        num2 = num1 - num2;  // b = (a + b) - b   b = a
+        num1 = num1 - num2; //  a = a - b    a = (a + b) - a   a = b
 
 
         System.out.println("num1 = " + num1);
