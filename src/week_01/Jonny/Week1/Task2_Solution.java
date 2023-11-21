b81cf61c37957ee63bb6287829d578b4579e7dc1
Jonny <jonny5891@yahoo.com>
2023-11-21 12:51:37 -0500

    committed tasks

---
+++ src/week_01/Jonny/Week1/Task2_Solution.java
@@ -0,0 +1,20 @@
+package week_01.Jonny.Week1;
+
+public class Task2_Solution {
+    public static void main(String[] args) {
+
+
+        int n1 = 1;
+        int n2 = 20;
+        int result = 0;
+
+        while (n1 <= n2) {
+            n2-=n1;//num2-num1=result
+            result+=1;//result= amount of times result-num1 can be done, repeat till not longer and add the amount of succesful attemps
+
+        }
+        System.out.println(result);
+    }
+}
+
+
