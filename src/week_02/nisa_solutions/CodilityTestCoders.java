402b9778fc3b1ae9a7d769aad1fa0df763d0b370
hayrunnisa00 <apaydinhayrunnisa@gmail.com>
2023-11-20 22:13:37 -0500

    created CodilityTestCoders.java

---
+++ src/week_02/nisa_solutions/CodilityTestCoders.java
@@ -0,0 +1,55 @@
+package week_02.nisa_solutions;
+
+import java.util.Scanner;
+
+public class CodilityTestCoders {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a positive number");
+        int n = scanner.nextInt();
+
+
+        CodilityTestCoders(n);
+
+
+
+    }
+
+    private static void CodilityTestCoders(int n) {
+
+        String divisible2 = "Codility";
+        String divisible3 = "Test";
+        String divisible5 = "Coders";
+        String result = "";
+
+        for (int i = 1; i <= n ; i++) {
+
+            if (i%2!=0 && i%3!=0 && i%5!=0){
+                result += "" + i;
+            }
+            if (i % 2 == 0) {
+                result += divisible2;
+            }
+            if (i % 3 == 0) {
+                result += divisible3;
+            }
+            if (i % 5 == 0) {
+                result += divisible5;
+
+            }
+            System.out.println(result);
+                result = "";
+
+
+
+        }
+
+
+
+    }
+
+
+}
