5ac250e8820ef38c1f8c5311a4fa9d03e2c9ccaf
Lokmanbzd <l.bouzerde@gmail.com>
2023-12-08 21:29:03 -0600

    Updated Unique Reverse class

--- src/week_05/lokmanSolutions/Reverse.java
+++ src/week_05/lokmanSolutions/Reverse.java
@@ -1,4 +1,30 @@
 package week_05.lokmanSolutions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Reverse {
+
+    public static void main(String[] args) {
+
+        System.out.println(revers("ABCD"));
+
+    }
+
+    public static String revers(String str){
+
+        String result = "";
+
+        for (int i = str.length()-1; i >= 0; i--) {
+            result += str.charAt(i);
+        }
+
+        return result;
+
+    }
+
 }
+/*
+Write a return method that can reverse String
+Ex: Reverse("ABCD"); ==> DCBA
+ */
