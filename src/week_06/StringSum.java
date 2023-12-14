1f58c41982270f0fdd93da7276515e75c5509d5a
srestr53 <sangil9091@gmail.com>
2023-12-14 00:28:33 -0800

    add password StringSum Numbers solution

---
+++ src/week_06/StringSum.java
@@ -0,0 +1,33 @@
+package week_06;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+//String --sum of digits in a string
+// Write a method that can return the sum of the digits in a string
+public class StringSum {
+
+    public static void main(String[] args) {
+
+        String str = "Cydeo2023";
+        System.out.println("sumDigits(str) = " + sumDigits(str));
+
+    }
+
+    public static int sumDigits(String str) {
+
+        int sum = 0;
+
+        for (char each : str.toCharArray()) {
+
+            if (Character.isDigit(each)) {
+                sum += each-48;
+            }
+        }
+
+        return sum;
+    }
+
+
+}
