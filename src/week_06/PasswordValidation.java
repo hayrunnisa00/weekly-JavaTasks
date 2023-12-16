79fcd13ec730577ba4459955cdb8366cf298cd7a
Lokmanbzd <l.bouzerde@gmail.com>
2023-12-15 22:18:22 -0600

    Updated PasswordValidation class

--- src/week_06/PasswordValidation.java
+++ src/week_06/PasswordValidation.java
@@ -1,4 +1,35 @@
 package week_06;
 
 public class PasswordValidation {
+
+    public static void main(String[] args) {
+
+        String password = "asdR.f4654";
+
+        System.out.println(verifyPassword(password));
+
+
+    }
+
+    public static boolean verifyPassword(String password) {
+
+        boolean validPassword = false;
+
+        if (password.length() >= 6 && !password.contains(" ")) {
+
+            for (int i = 0; i < password.length(); i++) {
+
+                char ch = password.charAt(i);
+
+                if (Character.isUpperCase(ch) || Character.isLowerCase(ch) || !Character.isLetterOrDigit(ch) || Character.isDigit(ch)) {
+                    validPassword = true;
+                }
+
+
+            }
+
+        }
+        return validPassword;
+
+    }
 }
