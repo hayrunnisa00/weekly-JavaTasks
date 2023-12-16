c0243bbcaa8b55beef81839901ac14f0009ab43a
Lokmanbzd <l.bouzerde@gmail.com>
2023-12-15 22:37:33 -0600

    updated Sum of digits in String class

--- src/week_06/lokmanSolution/SumOfDigitsInString.java
+++ src/week_06/lokmanSolution/SumOfDigitsInString.java
@@ -1,4 +1,23 @@
 package week_06.lokmanSolution;
 
 public class SumOfDigitsInString {
+
+    public static void main(String[] args) {
+        String str="A12B34C56";
+        System.out.println(sumOfDigits(str));
+
+    }
+    public static int sumOfDigits(String str ){
+        int sum=0;
+        for (int i = 0; i < str.length(); i++) {
+            char ch=str.charAt(i);
+            if(Character.isDigit(ch)){
+                sum +=ch-48;
+            }
+
+        }
+        return sum;
+    }
+
+
 }
