cc40d950cbffbf5c77b2cc9a113b4664f53ca826
Lokmanbzd <l.bouzerde@gmail.com>
2023-12-15 22:16:07 -0600

    updated MaxNumber class

--- src/week_06/MaxNumber.java
+++ src/week_06/MaxNumber.java
@@ -1,4 +1,32 @@
 package week_06;
 
 public class MaxNumber {
+
+    public static void main(String[] args) {
+
+        int[] arr = {5,10,78,62,61,2,5};
+        maxNum(arr);
+
+    }
+
+
+    public static void maxNum(int[] arr){
+
+        int max = Integer.MIN_VALUE;
+
+        for (int each: arr){
+
+            if (each> max){
+                max = each;
+            }
+
+        }
+        System.out.println(max);
+
+    }
+
+
 }
+/*
+Array -- Find Maximum Write a method that can find the maximum number from an int Array
+ */
