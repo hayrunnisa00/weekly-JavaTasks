d54879f7d112bc21f8719d82b516c7ba0cfad493
hayrunnisa00 <apaydinhayrunnisa@gmail.com>
2024-01-02 07:56:56 -0500

    added new method MoveToZero

--- src/week_08/nisa_solutions/MoveZeros.java
+++ src/week_08/nisa_solutions/MoveZeros.java
@@ -10,6 +10,9 @@ public class MoveZeros {
 
 
         System.out.println(Arrays.toString(moveZeroEnd(arr)));
+        System.out.println(Arrays.toString(moveZero(arr)));
+        System.out.println(Arrays.toString(zero(arr)));
+
 
     }
 
@@ -24,10 +27,35 @@ public class MoveZeros {
                 zeroEnd[j] = arr[i];
                 j++;
             }
-
         }
-
-
         return zeroEnd;
     }
+
+
+    public static String[] moveZero(int[] arr){
+
+        String num = "";
+        String zero = "";
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0){
+                zero += arr[i];
+            }else {
+                num += arr[i];
+            }
+        }
+        return (num + zero).split("");
+    }
+
+
+    public static int[] zero(int[] arr){
+        Arrays.sort(arr);
+        int[] sorted = new int[arr.length];
+        for (int i = arr.length-1, j=0; i >= 0; i--, j++) {
+            sorted[j] = arr[i];
+        }
+        return sorted;
+    }
+
+
 }
