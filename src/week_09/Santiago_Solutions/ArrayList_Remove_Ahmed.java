3b1834f3b8f1340351d6c747b027d8a9ec2b5100
srestr53 <sangil9091@gmail.com>
2024-01-12 15:55:08 -0800

    week_09 solutions

---
+++ src/week_09/Santiago_Solutions/ArrayList_Remove_Ahmed.java
@@ -0,0 +1,11 @@
+package week_09.Santiago_Solutions;
+
+public class ArrayList_Remove_Ahmed {
+
+
+
+
+}
+
+/*2) ArrayList - Remove "Ahmed"
+Given a list of people' names: "Ahmed", "John", Eric", "Ahmed"..... Write a java operation to remove all the names named Ahmed*/
---
+++ src/week_09/Santiago_Solutions/ArrayList_Remove_Some_values.java
@@ -0,0 +1,9 @@
+package week_09.Santiago_Solutions;
+
+public class ArrayList_Remove_Some_values {
+
+
+}
+/*3) ArrayList - Remove some values
+Given a list of Integers 1, 2, 3, 4, 5, 6 ....etc. remove all values greater than 100.*/
+
