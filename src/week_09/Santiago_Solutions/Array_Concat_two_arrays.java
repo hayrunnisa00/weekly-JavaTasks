f839f811b90e6f0fad699dce04ff01435bc7e14e
srestr53 <sangil9091@gmail.com>
2024-01-12 16:36:51 -0800

    Solution arrayConcatenate

--- src/week_09/Santiago_Solutions/Array_Concat_two_arrays.java
+++ src/week_09/Santiago_Solutions/Array_Concat_two_arrays.java
@@ -1,6 +1,9 @@
 package week_09.Santiago_Solutions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Array_Concat_two_arrays {
 
@@ -9,9 +12,11 @@ public class Array_Concat_two_arrays {
         int[] arr1 = {1, 2, 3, 4, 5};
         int[] arr2 = {6, 7, 8, 9, 10};
 
-        int[] arr3 = concatTwoArrays1(arr1, arr2);
+        System.out.println(Arrays.toString(concatTwoArrays1(arr1, arr2)));
 
-        System.out.println(Arrays.toString(arr3));
+        System.out.println("=======================================");
+
+
     }
 
 
@@ -32,21 +37,12 @@ public class Array_Concat_two_arrays {
         }
 
 
-
         return arr3;
 
     }
 
-    @Override
-    public static int[] concatTwoArrays1(Integer[] arr1, Integer[] arr2) {
-
-        int[] arr3 = new int[arr1.length + arr2.length];
-
-        return arr3;
-
-    }
 
 }
 
 /*1) Array - Concat two arrays
-Write a return method that can concate two arrays*/
+Write a return method that can concatenate two arrays*/
