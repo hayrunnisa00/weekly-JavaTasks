69f3e815f3218981793cf54521f90066e7a7af83
vandalat <140336926+vandalat@users.noreply.github.com>
2024-01-17 11:46:44 -0600

    Commit week 9 van solutions

--- src/week_09/Van_soluiton/RemoveSomeValue.java
+++ src/week_09/Van_soluiton/RemoveSomeValue.java
@@ -8,25 +8,25 @@ import java.util.List;
 public class RemoveSomeValue {
     public static void main(String[] args) {
         List<Integer> list=new ArrayList<>(Arrays.asList(5,1,2,3,4,7,8,44,33,2,11,567,889,43,664));
-
-        System.out.println(removeValue(list));
-        System.out.println(removeValue1(list));
+        int num =100;
+        System.out.println(removeValue(list,num));
+        System.out.println(removeValue1(list,num));
     }
-    public static List<Integer> removeValue(List<Integer> list){
+    public static List<Integer> removeValue(List<Integer> list, int num){
         List<Integer> newList=new ArrayList<>(list);
         Iterator<Integer> it = newList.iterator();
         while (it.hasNext()){
             Integer each = (Integer) it.next();
-            if (each>100){
+            if (each>num){
                 it.remove();
             }
         }
 
         return newList;
     }
-    public static List<Integer> removeValue1(List<Integer> list){
+    public static List<Integer> removeValue1(List<Integer> list, int num){
 
-            list.removeIf(k->k>100);
+            list.removeIf(k->k>num);
 
         return list;
 
